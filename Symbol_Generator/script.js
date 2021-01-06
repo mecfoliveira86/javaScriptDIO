9d9bfea1b64f443fbf1ac87534d95f359b3c104c
Felipe Oliveira <mec.foliveira@hotmail.com>
2021-01-06 08:49:25 -0300

    Content related to Symbols and Generators

--- Symbol_Generator/script.js
+++ Symbol_Generator/script.js
@@ -8,6 +8,7 @@ const uniqueID = Symbol('Hello');
 //  a palavra Hello é para debug somente
 
 //console.log(uniqueID === uniqueID2);
+<<<<<<< HEAD
 
 //  comparando os dois symbols com a mesma string identificadora.
 
@@ -54,4 +55,51 @@ for (let value of arr) {
 
 //Symbol and Generator
 
+=======
+
+//  comparando os dois symbols com a mesma string identificadora.
+
+//Symbol é utilizado para identificar itens, não ser acessado se não for intencionalmente.
+
+const obj = {
+    [uniqueID]:'Hello'
+}
+
+console.log(obj);
+
+Object.keys(obj);
+//  listar chaves do meu objeto
+
+Object.getOwnPropertySymbols(obj);
+// listar chaves em formato Symbol específicas do meu objeto
+
+//  Symbols possuem uma série de propriedades denominadas Well Known Symbols
+
+const arr = [1, 2, 3, 4];
+
+//percorrendo o array por um while
+const it = arr[Symbol.iterator]();
+
+console.log("Percorrendo Array por While:")
+while (true) {
+    let {value, done} = it.next();
+    
+    if(done) {
+        break;
+    }
+
+    console.log(value);
+   
+}
+
+//percorrendo o array por for of (nativo do JS)
+
+console.log("Percorrendo Array por For of:")
+
+for (let value of arr) {
+    console.log(value);
+}
+
+//Symbol and Generator
+>>>>>>> fcbf99c (Content related to Symbols and Generators)
 
